3b87d441bdd7571e8af2922a36cea09ae87a39bf
Christoph Caprano <Christoph.Caprano@valiton.com>
2017-03-27 09:00:59 +0200

    Add support for bash commands

--- src/sql/magic.py
+++ src/sql/magic.py
@@ -1,5 +1,6 @@
 import re
 from IPython.core.magic import Magics, magics_class, cell_magic, line_magic, needs_local_scope
+
 try:
     from traitlets.config.configurable import Configurable
     from traitlets import Bool, Int, Unicode
@@ -17,9 +18,10 @@ from sqlalchemy.exc import ProgrammingError, OperationalError
 import sql.connection
 import sql.parse
 import sql.run
+import subprocess
 
 
-def _replace_pattern(s, kw, pattern=re.compile(r'\$(\w+)')):
+def _replace_pattern_with_ns(s, kw, pattern=re.compile(r'\$(\w+)')):
     """
     Replace delimited keys in a string with values from kw.
 
@@ -28,6 +30,18 @@ def _replace_pattern(s, kw, pattern=re.compile(r'\$(\w+)')):
     lookup = lambda match: kw.get(match.group(1), match.group())
     return pattern.sub(lookup, s)
 
+
+def _replace_pattern_with_bash(source, pattern=re.compile(r'\$\{(\w+)\}')):
+    """
+    Replace delimited keys in a string with the evaluation of bash commands (command is in the square bracket).
+
+    Any pattern may be used, as long as the first group defines the lookup key.
+    """
+    lookup = lambda match: subprocess.Popen("echo %s " % match.group(1), stdout=subprocess.PIPE).stdout.read()
+
+    return pattern.sub(lookup, source)
+
+
 @magics_class
 class SqlMagic(Magics, Configurable):
     """Runs SQL statement on a database, specified by SQLAlchemy connect string.
@@ -35,17 +49,18 @@ class SqlMagic(Magics, Configurable):
     Provides the %%sql magic."""
 
     autolimit = Int(0, config=True, help="Automatically limit the size of the returned result sets")
-    style = Unicode('DEFAULT', config=True, help="Set the table printing style to any of prettytable's defined styles (currently DEFAULT, MSWORD_FRIENDLY, PLAIN_COLUMNS, RANDOM)")
+    style = Unicode('DEFAULT', config=True,
+                    help="Set the table printing style to any of prettytable's defined styles (currently DEFAULT, MSWORD_FRIENDLY, PLAIN_COLUMNS, RANDOM)")
     short_errors = Bool(True, config=True, help="Don't display the full traceback on SQL Programming Error")
-    displaylimit = Int(0, config=True, help="Automatically limit the number of rows displayed (full result set is still stored)")
+    displaylimit = Int(0, config=True,
+                       help="Automatically limit the number of rows displayed (full result set is still stored)")
     autopandas = Bool(False, config=True, help="Return Pandas DataFrames instead of regular result sets")
     column_local_vars = Bool(False, config=True, help="Return data into local variables from column names")
     feedback = Bool(True, config=True, help="Print number of rows affected by DML")
     dsn_filename = Unicode('odbc.ini', config=True, help="Path to DSN file. "
-                           "When the first argument is of the form [section], "
-                           "a sqlalchemy connection string is formed from the "
-                           "matching section in the DSN file.")
-
+                                                         "When the first argument is of the form [section], "
+                                                         "a sqlalchemy connection string is formed from the "
+                                                         "matching section in the DSN file.")
 
     def __init__(self, shell):
         Configurable.__init__(self, config=shell.config)
@@ -87,7 +102,8 @@ class SqlMagic(Magics, Configurable):
         user_ns.update(local_ns)
 
         parsed = sql.parse.parse('%s\n%s' % (line, cell), self)
-        parsed['sql'] = _replace_pattern(parsed['sql'], self.shell.user_ns)
+        parsed['sql'] = _replace_pattern_with_ns(parsed['sql'], self.shell.user_ns)
+        parsed['sql'] = _replace_pattern_with_bash(parsed['sql'])
         flags = parsed['flags']
         conn = sql.connection.Connection.get(parsed['connection'])
 
@@ -98,8 +114,8 @@ class SqlMagic(Magics, Configurable):
             result = sql.run.run(conn, parsed['sql'], self, user_ns)
 
             if result is not None and not isinstance(result, str) and self.column_local_vars:
-                #Instead of returning values, set variables directly in the
-                #users namespace. Variable names given by column names
+                # Instead of returning values, set variables directly in the
+                # users namespace. Variable names given by column names
 
                 if self.autopandas:
                     keys = result.keys()
@@ -122,7 +138,7 @@ class SqlMagic(Magics, Configurable):
                     self.shell.user_ns.update({result_var: result})
                     return None
 
-                #Return results into the default ipython _ variable
+                # Return results into the default ipython _ variable
                 return result
 
         except (ProgrammingError, OperationalError) as e:
@@ -133,6 +149,7 @@ class SqlMagic(Magics, Configurable):
                 raise
 
     legal_sql_identifier = re.compile(r'^[A-Za-z0-9#_$]+')
+
     def _persist_dataframe(self, raw, conn, user_ns):
         """Implements PERSIST, which writes a DataFrame to the RDBMS"""
         if not DataFrame:
@@ -147,7 +164,7 @@ class SqlMagic(Magics, Configurable):
         if not isinstance(frame, DataFrame) and not isinstance(frame, Series):
             raise TypeError('%s is not a Pandas DataFrame or Series' % frame_name)
 
-       # Make a suitable name for the resulting database table
+            # Make a suitable name for the resulting database table
         table_name = frame_name.lower()
         table_name = self.legal_sql_identifier.search(table_name).group(0)
 
